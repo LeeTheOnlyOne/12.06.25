4599f7d037a1179fb029c0e5125f9d8059159d70
Denis B. <hereisnobody5@gmail.com>
2025-06-19 10:51:22 +0300

    Add files via upload

---
+++ 19.6.2025/19.6.2025-SecondTask.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19._6._2025
+{
+    internal class _19
+    {
+        static void Main(string[] args)
+        {
+            string student = Console.ReadLine();
+            Console.WriteLine($"{student} -> ");
+            string book = Console.ReadLine();
+
+            Dictionary<string, List<string>> studentBooks = new Dictionary<string, List<string>>();
+
+            while (student != "end")
+            {
+                student = Console.ReadLine();
+                Console.WriteLine($"{student} -> ");
+                book = Console.ReadLine();
+
+                if (studentBooks.ContainsKey(student))
+                {
+                    studentBooks[student].Add(book);
+                }
+                else
+                {
+                    studentBooks[student] = new List<string> { book };
+                }
+
+            }
+
+            foreach (var entry in book)
+            {
+                Console.WriteLine($"{entry}: {book}");
+            }
+        }
+    }
+}
---
+++ 19.6.2025/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _19._6._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] parts = line.Split("");
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            while (line != "stop")
+            {
+                line = Console.ReadLine();
+                foreach (string part in parts)
+                {
+                    if (wordCount.ContainsKey(part))
+                    {
+                        wordCount[part]++;
+                    }
+                    else
+                    {
+                        wordCount[part] = 1;
+                    }
+                } 
+            }
+
+            foreach (var name in wordCount)
+            {
+                Console.WriteLine($"{name.Key} -> {name.Value}");
+            }
+
+            // No idea, doesn't work correctly, Mimi somehow has 5
+
+        }
+    }
+}
